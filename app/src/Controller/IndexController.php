17fb26cfa1f09ce5135e30bc6437ebdbafcb60e8
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2023-11-02 20:08:46 +0100

    Add form

--- app/src/Controller/IndexController.php
+++ app/src/Controller/IndexController.php
@@ -4,13 +4,38 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Form\RegistrationType;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 final class IndexController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request, EntityManagerInterface $em): Response
     {
-        return new Response('foo');
+        $form = $this->createForm(RegistrationType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isSubmitted()) {
+            $submittedToken = $request->request->get('_token');
+            if (!$this->isCsrfTokenValid('registration', $submittedToken)) {
+                throw $this->createAccessDeniedException('Invalid CSRF token');
+            }
+
+            $registration = $form->getData();
+            $registration->setCreatedAt(new DateTimeImmutable());
+            $em->persist($registration);
+            $em->flush();
+
+            $this->addFlash('success', 'Thank you for registering!');
+            return $this->redirectToRoute('app');
+        }
+
+        return $this->render('index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ app/src/Form/RegistrationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Registration;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('emailAddress', EmailType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Registration::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id'   => 'registration',
+        ]);
+    }
+}
